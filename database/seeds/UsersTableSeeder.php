6d0a94e31703f49e2a03c13ff13748fe0ba1ab5d
whoisnian <zhuchangbao2017@gmail.com>
2017-05-26 15:03:41 +0800

    user seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,14 +15,18 @@ class UsersTableSeeder extends Seeder
             'privilege' => '2',
             'name' => 'admin',
             'email' => 'admin',
-            'password' => Hash::make('admin'),
+            'password' => Hash::make('adminadmin'),
+            'created_at' => '2000-01-01 00:00:00',
+            'updated_at' => '2000-01-01 00:00:00',
         ]);
 
         DB::table('users')->insert([
             'privilege' => '0',
             'name' => 'guest',
             'email' => 'guest',
-            'password' => Hash::make('guest'),
+            'password' => Hash::make('guestguest'),
+            'created_at' => '2000-01-01 00:00:00',
+            'updated_at' => '2000-01-01 00:00:00',
         ]);
     }
 }
